a26d9e813d3f0cecf48f732cadd45299c5f14295
Syed Javith <syedjavith14@gmail.com>
2023-09-06 12:25:35 +0530

    frontend

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { UserProvider } from './contexts/userContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <App />
+  <React.StrictMode>
+   <UserProvider>
+        <App />
+    </UserProvider>
+  </React.StrictMode>,
 );
